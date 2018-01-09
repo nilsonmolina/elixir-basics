4e9ab2dbfccaa87a258240fbc8e5c1aea6fa34ad
nilsonmolina <molinanilson@gmail.com>
2018-01-09 12:13:13 -0800

    started identicon project

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,14 @@
+defmodule Identicon do
+
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
